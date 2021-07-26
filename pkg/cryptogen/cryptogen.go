22904214859de5c7b2e469327007e0d8687cd402
tinywell <tinywell@tinywell.com>
2021-07-26 14:25:02 +0800

    cryptogen

--- pkg/cryptogen/cryptogen.go
+++ pkg/cryptogen/cryptogen.go
@@ -1,9 +1,18 @@
 package cryptogen
 
-import "github.com/tinywell/baas/pkg/cryptogen/common"
+import (
+	"github.com/tinywell/baas/pkg/cryptogen/common"
+	"github.com/tinywell/baas/pkg/cryptogen/sw"
+)
 
 // Generator 证书颁发器
 type Generator interface {
-	GenerateOrgCA(org common.NodeSpec) (common.Organization, error)                       // 签发组织根证书
-	GenarateMember(member common.NodeSpec, CA common.Organization) (common.Member, error) // 签发组织成员证书
+	GenerateOrgCA(org *common.NodeSpec) (common.Organization, error)                        // 签发组织根证书
+	GenarateMember(member *common.NodeSpec, CA *common.Organization) (common.Member, error) // 签发组织成员证书
+}
+
+// NewCenarator ...
+func NewCenarator() Generator {
+	// TODO:
+	return &sw.Gen{}
 }
